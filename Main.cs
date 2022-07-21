9150daf3d702551637145cd422d70c27c4dcfabe
zonde306 <13820323+zonde306@users.noreply.github.com>
2022-07-21 14:08:17 +0800

    init

---
+++ Main.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using BepInEx;
+using BepInEx.IL2CPP;
+
+namespace RF5_MissionComplete
+{
+    [BepInPlugin(GUID, NAME, VERSION)]
+    [BepInProcess(GAME_PROCESS)]
+    public class Main : BasePlugin
+    {
+        #region PluginInfo
+        private const string GUID = "27ACD694-6AF8-3FD3-BEC0-32D0FCA1F8BF";
+        private const string NAME = "RF5_MissionComplete";
+        private const string VERSION = "1.0";
+        private const string GAME_PROCESS = "Rune Factory 5.exe";
+        #endregion
+
+        public override void Load()
+        {
+            new Harmony(GUID).PatchAll();
+        }
+    }
+}
---
+++ OrderManagerPatch.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+
+namespace RF5_MissionComplete
+{
+	[HarmonyPatch(typeof(OrderManager), nameof(OrderManager.OrderFailureByOrderId))]
+	public class OrderManagerPatch
+	{
+		static bool Prefix(int orderId, OrderManager __instance)
+		{
+			if (__instance.GetAcceptOrderData(orderId).IsComplete())
+				return true;	// 已完成则取消任务
+			else
+				__instance.CompletedAllOrder(orderId);	// 未完成则完成它
+
+			return false;
+		}
+	}
+}
