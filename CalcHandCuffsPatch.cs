1f76c88a2eebd41dc322562b8fd51f0222248074
zonde306 <13820323+zonde306@users.noreply.github.com>
2022-07-22 13:55:20 +0800

    catch always successful

---
+++ CalcHandCuffsPatch.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+
+namespace RF5_MissionComplete
+{
+	[HarmonyPatch(typeof(Calc.CalcHandCuffs), nameof(Calc.CalcHandCuffs.LotteryWantedMonster))]
+	public class CalcHandCuffsPatch
+	{
+		static bool Prefix(ref bool __result)
+		{
+			__result = true;
+			return false;
+		}
+	}
+}
