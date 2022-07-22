3fbe487853b140a74b6cddffddfe1f029490f69b
zonde306 <13820323+zonde306@users.noreply.github.com>
2022-07-22 12:02:17 +0800

    wanted monster

---
+++ WantedDataPatch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+
+namespace RF5_MissionComplete
+{
+	[HarmonyPatch(typeof(RF5WANTED.WantedData), nameof(RF5WANTED.WantedData.CheckMonster))]
+	public class WantedDataPatch
+	{
+		static bool Prefix(ref bool __result)
+		{
+			if (!RF5WANTED.WantedData.isAccepted)
+				return true;
+
+			__result = true;
+			return false;
+		}
+	}
+}
